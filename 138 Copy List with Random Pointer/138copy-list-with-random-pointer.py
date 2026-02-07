dc88a6c6aee4eda1a22393493ac1f173696fc406
Vedant <vedanthirekar@gmail.com>
2026-02-07 01:08:12 +0000

    Code challenge submission for question: 138 Copy List with Random Pointer

---
+++ 138 Copy List with Random Pointer/138copy-list-with-random-pointer.py
@@ -0,0 +1,27 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+"""
+
+class Solution:
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        if not head: return None
+        curr = head
+        h = {}
+        while curr:
+            newnode = Node(x = curr.val)
+            h[curr] = newnode
+            curr = curr.next
+
+        curr = head
+        while curr:
+            newnode = h[curr]
+            newnode.next = h[curr.next] if curr.next else None
+            newnode.random = h[curr.random] if curr.random else None
+            curr = curr.next
+
+        return h[head]
