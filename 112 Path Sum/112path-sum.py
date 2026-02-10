8c3de51b35b3b0e23a0ffc738ac3456c6c2c651e
Vedant <vedanthirekar@gmail.com>
2026-02-10 22:38:35 +0000

    Code challenge submission for question: 112 Path Sum

---
+++ 112 Path Sum/112path-sum.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        summ = 0
+
+        def traverse(root, summ):
+            if not root:
+                return False
+
+            summ += root.val
+            
+            if not root.left and not root.right:
+                return summ == targetSum 
+
+            
+
+            return traverse(root.left, summ) or traverse(root.right, summ)
+
+        return traverse(root, summ)
+         
+        
+
+
