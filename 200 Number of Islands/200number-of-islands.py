2e6195f051a0e9ded3213cd0cac729f47354f74f
Vedant <vedanthirekar@gmail.com>
2026-02-26 00:22:25 +0000

    Code challenge submission for question: 200 Number of Islands

---
+++ 200 Number of Islands/200number-of-islands.py
@@ -0,0 +1,24 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        
+        m = len(grid)
+        n = len(grid[0])
+
+        def dfs(i,j):
+            if i<0 or i>=m or j<0 or j>=n or grid[i][j] != '1':
+                return
+            else:
+                grid[i][j] = "0"
+                dfs(i,j+1)
+                dfs(i,j-1)
+                dfs(i+1,j)
+                dfs(i-1,j)
+
+        n_is = 0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == '1':
+                    n_is +=1
+                    dfs(i,j)
+
+        return n_is
