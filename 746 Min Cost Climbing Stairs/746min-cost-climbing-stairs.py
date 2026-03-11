771ed2d5332dfbc4935944085e372ee883f78f9a
Vedant <vedanthirekar@gmail.com>
2026-03-11 21:55:57 +0000

    Code challenge submission for question: 746 Min Cost Climbing Stairs

---
+++ 746 Min Cost Climbing Stairs/746min-cost-climbing-stairs.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        
+        n = len(cost)
+        prev = 0
+        curr = 0
+
+        for i in range(2, n+1):
+            prev, curr = curr , min(prev + cost[i-2], curr+cost[i-1])
+
+        return curr
