438b683075e9ba82cd4f113aeecd57d6b292c4cd
Vedant <vedanthirekar@gmail.com>
2026-03-15 02:50:04 +0000

    Code challenge submission for question: 53 Maximum Subarray

---
+++ 53 Maximum Subarray/53maximum-subarray.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        curr_sum = 0
+        max_sum = float("-inf")
+
+        for i in nums:
+            curr_sum += i
+            max_sum = max(max_sum, curr_sum)
+
+            if curr_sum <0:
+                curr_sum = 0
+        return max_sum
