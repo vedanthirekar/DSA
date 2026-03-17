de7a69b9be4e3c6414d8658f48fce2e0221aa0e6
Vedant <vedanthirekar@gmail.com>
2026-03-17 17:49:25 +0000

    Code challenge submission for question: 6 Zigzag Conversion

---
+++ 6 Zigzag Conversion/6zigzag-conversion.py
@@ -0,0 +1,25 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        n = len(s)
+        i = 0
+        d = 1
+
+        if numRows == 1:
+            return s
+
+        rows = [[] for _ in range(numRows)]
+
+        for char in s:
+            rows[i].append(char)
+            if i == 0:
+                d = 1
+            elif i == numRows-1:
+                d = -1
+            i += d
+
+        ret = ""
+        for i in range(numRows):
+            ret+= "".join(rows[i])
+
+        return ret
+            
