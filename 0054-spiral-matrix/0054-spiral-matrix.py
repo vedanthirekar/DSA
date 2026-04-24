338d7e87f5df7e3caf3643d6c82937cae8abef2a
Vedant Hirekar <78254671+vedanthirekar@users.noreply.github.com>
2026-04-24 17:22:24 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -12,20 +12,20 @@ class Solution:
             for i in range(l+1,r):
                 o.append(matrix[u+1][i])
             u+=1
-            if u+1 >= d:
-                break
+            # if u+1 >= d:
+            #     break
 
             for i in range(u+1,d):
                 o.append(matrix[i][r-1])
             r -=1
-            if l +1>= r:
-                break
+            # if l +1>= r:
+            #     break
 
             for i in range(r-1, l, -1):
                 o.append(matrix[d-1][i])
             d -=1
-            if u+1 >= d:
-                break
+            # if u+1 >= d:
+            #     break
 
             for i in range(d-1,u,-1):
                 o.append(matrix[i][l+1])
