35cae5392a5f51ca8d96ceec848bc509fcac0c19
Vedant Hirekar <78254671+vedanthirekar@users.noreply.github.com>
2026-04-26 15:58:22 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,5 +1,9 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
+        """if we check s[r] in hmap, and if its value is 0, still it will return True.
+        so instead i used this condition of hmap[s[r]]>0. 
+        """
+
         l = 0
         n = len(s)
         maxx = 0
