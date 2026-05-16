d40be2b7bb6f65a0442a2f70af6e6fc35cc66e88
Vedant Hirekar <vedanthirekar@gmail.com>
2026-05-16 15:34:39 -0400

    Time: 0 ms (100%), Space: 19.3 MB (61.65%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -3,14 +3,31 @@ class Solution:
         """at each step we can either rob the house or not rob the house. 
         we try to take the max of both. then we try to optimize this
         """
-        memo = {}
-        def dfs(n):
-            if n in memo:
-                return memo[n]
-            if n<0:
-                return 0
-            memo[n] =  max(dfs(n-1), dfs(n-2)+nums[n])
-            return memo[n]
-
         n = len(nums)
-        return dfs(n-1)
+        if not nums:
+            return 0
+        if n == 1:
+            return nums[0]
+        dp = [0]*n
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+        for i in range(2, n):
+            dp[i] = max(dp[i-1], dp[i-2]+nums[i])
+        return dp[-1]
+        
+        
+        
+        # memo = {}
+        # def dfs(n):
+        #     if n in memo:
+        #         return memo[n]
+        #     if n<0:
+        #         return 0
+        #     memo[n] =  max(dfs(n-1), dfs(n-2)+nums[n])
+        #     return memo[n]
+
+        # n = len(nums)
+        # return dfs(n-1)
+
+
+
