086d9805911948e052689eacde9c647ff55b46eb
Vedant Hirekar <vedanthirekar@gmail.com>
2026-05-16 15:45:06 -0400

    Time: 0 ms (100%), Space: 19.3 MB (24.06%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -3,17 +3,37 @@ class Solution:
         """at each step we can either rob the house or not rob the house. 
         we try to take the max of both. then we try to optimize this
         """
+
+        #space optimized
         n = len(nums)
         if not nums:
             return 0
         if n == 1:
             return nums[0]
-        dp = [0]*n
-        dp[0] = nums[0]
-        dp[1] = max(nums[0], nums[1])
-        for i in range(2, n):
-            dp[i] = max(dp[i-1], dp[i-2]+nums[i])
-        return dp[-1]
+        prev = nums[0]
+        curr = max(nums[1],nums[0])
+        res = 0
+        for i in range(2,n):
+            res = max(prev+nums[i],curr)
+            
+            prev = curr
+            curr = res
+            
+        return curr
+
+
+
+        # n = len(nums)
+        # if not nums:
+        #     return 0
+        # if n == 1:
+        #     return nums[0]
+        # dp = [0]*n
+        # dp[0] = nums[0]
+        # dp[1] = max(nums[0], nums[1])
+        # for i in range(2, n):
+        #     dp[i] = max(dp[i-1], dp[i-2]+nums[i])
+        # return dp[-1]
         
         
         
