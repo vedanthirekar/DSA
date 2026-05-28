90128c9d8677a96726a9ff63e30576506c0912f6
Vedant Hirekar <vedanthirekar@gmail.com>
2026-05-28 09:45:45 -0400

    Time: 0 ms (100%), Space: 19.5 MB (6.17%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -4,22 +4,49 @@ class Solution:
         we try to take the max of both. then we try to optimize this
         """
 
-        #space optimized
         n = len(nums)
-        if not nums:
-            return 0
-        if n == 1:
+        dp = [0]*n
+        if len(nums)<2:
             return nums[0]
-        prev = nums[0]
-        curr = max(nums[1],nums[0])
-        res = 0
+        
+        dp[0] = nums[0]
+        dp[1] = max(nums[1],nums[0])
+
         for i in range(2,n):
-            res = max(prev+nums[i],curr)
+            dp[i] = max(dp[i-1],dp[i-2]+nums[i])
+
+        return dp[n-1]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # #space optimized
+        # n = len(nums)
+        # if not nums:
+        #     return 0
+        # if n == 1:
+        #     return nums[0]
+        # prev = nums[0]
+        # curr = max(nums[1],nums[0])
+        # res = 0
+        # for i in range(2,n):
+        #     res = max(prev+nums[i],curr)
             
-            prev = curr
-            curr = res
+        #     prev = curr
+        #     curr = res
             
-        return curr
+        # return curr
 
 
 
