6c7bdd79bfd837555aef0d8184c4061a9ad9b960
Vedant Hirekar <vedanthirekar@gmail.com>
2026-05-31 17:57:43 -0400

    Time: 0 ms (100%), Space: 19.2 MB (93.38%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.py
@@ -0,0 +1,19 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        here we can see a pattern that if we swap the elements across the diagonal plane
+        and flip it across the vertical plane, we get the resulting matrix
+        """
+        
+        n = len(matrix)
+        for i in range(n):
+            for j in range(i+1,n):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+        for i in range(n):
+            for j in range(n//2):
+                matrix[i][j], matrix[i][n-1-j] = matrix[i][n-1-j], matrix[i][j]
+
+
+                
