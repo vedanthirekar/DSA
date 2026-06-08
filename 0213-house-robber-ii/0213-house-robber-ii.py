258d94f9b27e20312a90ed797bc8af45c409a765
Vedant Hirekar <vedanthirekar@gmail.com>
2026-06-08 12:45:08 -0400

    Time: 0 ms (100%), Space: 19.2 MB (92.9%) - LeetHub

---
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -0,0 +1,30 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        n = len(nums)
+        nums1 = nums[:n-1]
+        nums2 = nums[1:]
+
+
+        def max_amt(s_nums):
+            # if not s_nums:
+            #     return 0
+            s_n = n-1
+            if s_n<3:
+                return max(s_nums)
+            prev = s_nums[0]
+            curr = max(prev,s_nums[1])
+
+
+            for i in range(2,s_n):
+                temp = max(curr, prev+s_nums[i])
+                prev = curr
+                curr = temp
+
+            return curr 
+
+        if n ==1:
+            return nums[0]
+        a1 = max_amt(nums1)
+        a2 = max_amt(nums2)
+
+        return max(a1,a2)
