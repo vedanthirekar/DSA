1bbf926e403cdc80487d1f861b7e149b632db3d4
Vedant Hirekar <vedanthirekar@gmail.com>
2026-06-21 10:48:49 -0400

    Time: 953 ms (10.51%), Space: 22.8 MB (70.55%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -3,40 +3,45 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution:
+
+class Solution:    
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        
-        def mergetwo(l1,l2):
-            dummy = ListNode()
-            d = dummy
-            curr1 = l1
-            curr2 = l2
-            while curr1 and curr2:
-                if curr1.val<=curr2.val:
-                    d.next = curr1
-                    curr1 = curr1.next
-                else:
-                    d.next = curr2
-                    curr2 = curr2.next
-                d = d.next
-
-            if curr1:
-                d.next = curr1
-            else:
-                d.next = curr2
-            return dummy.next
-
-        n = len(lists)
+        # new_list = ListNode()
         if not lists:
             return None
-        if n<2:
-            return lists[0]
-        l1 = lists[0]
-        for i in range(1,n):
-            l1 = mergetwo(l1,lists[i])
 
-        return l1
+        while len(lists)>1:
+            list1 = lists.pop()
+            list2 = lists.pop()
+            new_list = self.mergetwolists(list1,list2)
+            lists.append(new_list)
 
+        return lists[-1]
+
+    
+    def mergetwolists(self, list1, list2):
+        dummy = ListNode()
+        curr = dummy
+        curr1 = list1
+        curr2 = list2
+        while curr1 and curr2:
+            if curr1.val<=curr2.val:
+                curr.next = curr1
+                curr = curr.next
+                curr1 = curr1.next
+            else:
+                curr.next = curr2
+                curr = curr.next
+                curr2 = curr2.next
+
+        if curr1:
+            curr.next = curr1
+        else:
+            curr.next = curr2
+
+        return dummy.next
+
+    
+    
 
         
-
