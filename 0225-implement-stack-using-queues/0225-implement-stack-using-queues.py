d5ffc4604eb774b00e272975c0670529219cc6ff
Vedant Hirekar <vedanthirekar@gmail.com>
2026-08-07 08:43:29 -0400

    Time: 0 ms (100%), Space: 19.4 MB (86.34%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.py
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.py
@@ -3,33 +3,61 @@ class MyStack:
 
     def __init__(self):
         self.q1 = deque()
-        self.q2 = deque()
+        # self.q2 = deque()
         self.size = 0
 
     def push(self, x: int) -> None:
+        # for i in range(self.size-1):
+        #     self.q1.append(self.q1.popleft())
+
         self.q1.append(x)
         self.size += 1
 
+        for i in range(self.size-1):
+            self.q1.append(self.q1.popleft())
+
+
     def pop(self) -> int:
-        for i in range(self.size -1):
-            self.q2.append(self.q1.popleft())
         res = self.q1.popleft()
-        self.q1 = self.q2
-        self.q2 = deque()
         self.size -=1
         return res
 
     def top(self) -> int:
-        for i in range(self.size -1):
-            self.q2.append(self.q1.popleft())
-        res = self.q1.popleft()
-        self.q2.append(res)
-        self.q1 = self.q2
-        self.q2 = deque()
-        return res
+        return self.q1[0]
 
     def empty(self) -> bool:
-        return True if self.size==0 else False
+        return self.size == 0
+
+
+    # def __init__(self):
+    #     self.q1 = deque()
+    #     self.q2 = deque()
+    #     self.size = 0
+
+    # def push(self, x: int) -> None:
+    #     self.q1.append(x)
+    #     self.size += 1
+
+    # def pop(self) -> int:
+    #     for i in range(self.size -1):
+    #         self.q2.append(self.q1.popleft())
+    #     res = self.q1.popleft()
+    #     self.q1 = self.q2
+    #     self.q2 = deque()
+    #     self.size -=1
+    #     return res
+
+    # def top(self) -> int:
+    #     for i in range(self.size -1):
+    #         self.q2.append(self.q1.popleft())
+    #     res = self.q1.popleft()
+    #     self.q2.append(res)
+    #     self.q1 = self.q2
+    #     self.q2 = deque()
+    #     return res
+
+    # def empty(self) -> bool:
+    #     return True if self.size==0 else False
 
 
 # Your MyStack object will be instantiated and called as such:
