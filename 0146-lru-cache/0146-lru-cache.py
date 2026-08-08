e43c0466ebdf57192a444a40747367da51289b61
Vedant Hirekar <vedanthirekar@gmail.com>
2026-08-08 17:16:00 -0400

    Time: 120 ms (59.54%), Space: 78.6 MB (29.13%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,65 +1,67 @@
 class LRUCache:
 
     def __init__(self, capacity: int):
+        self.start = Node()
+        self.end = Node()
+        self.start.nextt = self.end
+        self.end.prev = self.start
+        self.dict1 = {}
+        self.cap = 0
         self.capacity = capacity
-        self.count = 0
-        self.left = ListNode()
-        self.right = ListNode()
-        self.left.next = self.right
-        self.right.prev = self.left
-        self.mapper = defaultdict(ListNode)
 
     def get(self, key: int) -> int:
-        if key not in self.mapper:
-            return -1
-
-        node = self.mapper[key]
-        return_val = node.val
-        self.remove(node)
-        self.insert(node)
-        return return_val
-
-    def put(self, key: int, value: int) -> None:
-        # if self.count>=capacity:
-        if key not in self.mapper:
-            node = ListNode(key= key, val = value)
-            self.mapper[key] = node
+        if key in self.dict1:
+            node = self.dict1[key]
+            self.remove(node)
             self.insert(node)
-            self.count +=1
+            return node.val
 
         else:
-            node = self.mapper[key]
-            self.remove(node)
+            return -1
 
+    def put(self, key: int, value: int) -> None:
+        if key in self.dict1:
+            node = self.dict1[key]
             node.val = value
+            self.remove(node)
             self.insert(node)
+            # dict1[key] = node
 
-        if self.count>self.capacity:
-            lru_node = self.right.prev
-            self.remove(lru_node)
-            self.mapper.pop(lru_node.key)
-            self.count-=1
+        else:
+            node = Node()
+            node.val = value
+            node.key = key
+            self.insert(node)
+            self.cap += 1
+            self.dict1[key] = node
+
+            if self.cap>self.capacity:
+                node = self.end.prev
+                self.remove(node)
+                del self.dict1[node.key]
+                self.cap-=1
+
 
     def remove(self, node):
         prev = node.prev
-        next_n = node.next
-        prev.next = next_n
-        next_n.prev = prev
+        nextt = node.nextt
+        prev.nextt = nextt
+        nextt.prev = prev
 
     def insert(self, node):
-        temp = self.left.next
-        self.left.next = node
-        node.next = temp
-        node.prev = self.left
-        temp.prev = node
-        # node.prev = 
-
-class ListNode:
-    def __init__(self,key = -1, val = -1, prev = None, next = None):
+        node.prev = self.start
+        node.nextt = self.start.nextt
+        node.nextt.prev = node
+        self.start.nextt = node
+        
+            
+class Node:
+    def __init__(self, key = -1, val = -1, nextt = None , prev = None):
         self.key = key
         self.val = val
+        self.nextt = nextt
         self.prev = prev
-        self.next = next
+
 
 
 # Your LRUCache object will be instantiated and called as such:
