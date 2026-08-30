5fe298c029da83fc30c71cc3cf1195a0705fb36d
Vedant Hirekar <vedanthirekar@gmail.com>
2026-08-30 01:53:16 -0400

    Time: 14 ms (45.21%), Space: 23.9 MB (26.44%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,16 +1,18 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        dictt = defaultdict(list)
+        
+        groups = defaultdict(list)
+        res = []
+
         for s in strs:
-            charset = [0]*26
-            for i in s:
-                charset[ord(i)-97] +=1
-            
-            dictt[tuple(charset)].append(s)
+            s_map = [0]*26
 
-        l = []
-        for k in dictt:
-            l.append(dictt[k])
+            for letter in s:
+                s_map[ord(letter)-ord("a")] +=1
 
-        return l
-        # return dictt[key] for key in dictt 
+            groups[tuple(s_map)].append(s)
+
+        for value in groups.values():
+            res.append(value)
+
+        return res
