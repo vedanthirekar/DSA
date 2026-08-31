839334a4a6186a13e84b84c14bee6dcb87354a43
Vedant Hirekar <vedanthirekar@gmail.com>
2026-08-31 19:35:22 -0400

    Time: 0 ms (100%), Space: 19.4 MB (24.47%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,24 +1,35 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         
-        #dp cache 
         n = len(nums)
-        memo = {}
-        def dfs(i):
-            if i >= n:
-                return 0
+        dp = [0]*(n+2)
 
-            if i in memo:
-                return memo[i]
+        # dp[]
+        for i in range(n-1, -1, -1):
+            print(i)
+            dp[i] = max(nums[i]+dp[i+2], dp[i+1])
+
+        return dp[0]
+
+
+        # #dp cache 
+        # n = len(nums)
+        # memo = {}
+        # def dfs(i):
+        #     if i >= n:
+        #         return 0
+
+        #     if i in memo:
+        #         return memo[i]
             
-            memo[i] = max(nums[i] + dfs(i+2), dfs(i+1))
-            return memo[i]
+        #     memo[i] = max(nums[i] + dfs(i+2), dfs(i+1))
+        #     return memo[i]
 
-        return dfs(0)
+        # return dfs(0)
 
 
 
-        #recursive 
+        # # recursive 
         # n = len(nums)
         # def dfs(i):
         #     if i >= n:
