00da223474b811d3cdda8a4440ad64b14e3eb5cf
Vedant Hirekar <vedanthirekar@gmail.com>
2026-08-31 19:29:54 -0400

    Time: 0 ms (100%), Space: 19.3 MB (24.47%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,82 +1,30 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        """at each step we can either rob the house or not rob the house. 
-        we try to take the max of both. then we try to optimize this
-        """
-
+        
+        #dp cache 
         n = len(nums)
-        # dp = [0]*n
-        if len(nums)<2:
-            return nums[0]
-        
-        prev = nums[0]
-        curr = max(nums[1],nums[0])
+        memo = {}
+        def dfs(i):
+            if i >= n:
+                return 0
 
-        for i in range(2,n):
-            temp = curr
-            curr = max(prev+nums[i],curr)
-            prev = temp
-
-        return curr
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        # #space optimized
-        # n = len(nums)
-        # if not nums:
-        #     return 0
-        # if n == 1:
-        #     return nums[0]
-        # prev = nums[0]
-        # curr = max(nums[1],nums[0])
-        # res = 0
-        # for i in range(2,n):
-        #     res = max(prev+nums[i],curr)
+            if i in memo:
+                return memo[i]
             
-        #     prev = curr
-        #     curr = res
-            
-        # return curr
+            memo[i] = max(nums[i] + dfs(i+2), dfs(i+1))
+            return memo[i]
+
+        return dfs(0)
 
 
 
+        #recursive 
         # n = len(nums)
-        # if not nums:
-        #     return 0
-        # if n == 1:
-        #     return nums[0]
-        # dp = [0]*n
-        # dp[0] = nums[0]
-        # dp[1] = max(nums[0], nums[1])
-        # for i in range(2, n):
-        #     dp[i] = max(dp[i-1], dp[i-2]+nums[i])
-        # return dp[-1]
-        
-        
-        
-        # memo = {}
-        # def dfs(n):
-        #     if n in memo:
-        #         return memo[n]
-        #     if n<0:
+        # def dfs(i):
+        #     if i >= n:
         #         return 0
-        #     memo[n] =  max(dfs(n-1), dfs(n-2)+nums[n])
-        #     return memo[n]
+            
+        #     maxx = max(nums[i] + dfs(i+2), dfs(i+1))
+        #     return maxx
 
-        # n = len(nums)
-        # return dfs(n-1)
-
-
-
+        # return dfs(0)
