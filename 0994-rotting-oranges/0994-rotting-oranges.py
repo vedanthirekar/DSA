7bfce18da496ab45f89fad9c343228576a73768c
Vedant Hirekar <vedanthirekar@gmail.com>
2026-08-31 01:59:17 -0400

    Time: 6 ms (18.86%), Space: 19.3 MB (79.21%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,46 +1,40 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
+        
+        queue = deque()
+        count = 0
+        fresh = 0
+        # seen = set()
 
-        def helper(r,c):
-            if r>=0 and c>=0 and r<rows and c<cols and grid[r][c] == 1:
-                grid[r][c] = 2
-                queue.append((r,c))
-
-            
         rows = len(grid)
         cols = len(grid[0])
-        queue = deque()
-        seen = set()
-        total_orange = 0
-        time = 0
 
+        def bfs(r,c):
+            nonlocal fresh
+            if r>=0 and c>=0 and c<cols and r<rows and grid[r][c] == 1:
+                grid[r][c] = 2
+                queue.append((r,c))
+                fresh -=1
 
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == 2:
-                    seen.add((r,c))
                     queue.append((r,c))
-                    total_orange +=1
+                    # seen.add((r,c))
+                elif grid[r][c] == 1:
+                    fresh += 1
 
-                if grid[r][c] ==1:
-                    total_orange +=1
-        if total_orange == 0:
-            return 0
-        while queue:
-            time+=1
-            for _ in range(len(queue)):
+        while queue and fresh>0:
+            l = len(queue)
+            count +=1
+            for i in range(l):
                 r,c = queue.popleft()
-                seen.add((r,c))
-                helper(r-1,c)
-                helper(r+1,c)
-                helper(r,c+1)
-                helper(r,c-1)
+                bfs(r,c-1)
+                bfs(r,c+1)
+                bfs(r-1,c)
+                bfs(r+1,c)
 
 
-        if len(seen)==total_orange:
-            return time-1
-        else:
-            return -1
+        return count if not fresh else -1
 
-
-
+        
