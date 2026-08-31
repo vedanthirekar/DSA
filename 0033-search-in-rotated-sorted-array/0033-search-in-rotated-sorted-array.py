f71d3eb31c680bd7b51f55bd187816457ed57602
Vedant Hirekar <vedanthirekar@gmail.com>
2026-08-31 16:50:07 -0400

    Time: 0 ms (100%), Space: 19.7 MB (10.44%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,61 +1,29 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
+        
+        l = 0
+        r = len(nums)-1
 
-            l = 0
-            h = len(nums)-1
-            m = 0
-            while l <h:
-                m = l+ (h-l)//2
-                if nums[m]<nums[h]:
-                    h = m
+        while l<=r:
+
+            mid = l+(r-l)//2
+
+            if nums[mid] == target:
+                return mid
+
+            if nums[mid]<nums[r]:
+                if nums[mid]< target <= nums[r]:
+                    l = mid + 1
+
                 else:
-                    l = m+1
-            # print (m)
+                    r = mid-1
 
-            pivot = l
-            h = len(nums)-1
-            l = 0
-            if target>=nums[pivot] and target<=nums[h]:
-                l = pivot
             else:
-                h = pivot-1
-
-            while l<=h:
-                m = l+(h-l)//2
-                if nums[m]== target:
-                    return m
-                elif nums[m]>target:
-                    h=m-1
+                if nums[l]<= target< nums[mid]:
+                    r = mid -1
                 else:
-                    l= m+1
-            return -1
-                
-# class Solution:
-#     def search(self, nums: List[int], target: int) -> int:
-#         l, r = 0, len(nums) - 1
+                    l = mid + 1
 
-#         while l < r:
-#             m = (l + r) // 2
-#             if nums[m] > nums[r]:
-#                 l = m + 1
-#             else:
-#                 r = m
+        return -1
 
-#         pivot = l
-#         l, r = 0, len(nums) - 1
 
-#         if target >= nums[pivot] and target <= nums[r]:
-#             l = pivot
-#         else:
-#             r = pivot - 1
-
-#         while l <= r:
-#             m = (l + r) // 2
-#             if nums[m] == target:
-#                 return m
-#             elif nums[m] < target:
-#                 l = m + 1
-#             else:
-#                 r = m - 1
-
-#         return -1
