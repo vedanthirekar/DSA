2da80524ff2fbdfb452729b7d82708dbbcd81e7d
Vedant Hirekar <vedanthirekar@gmail.com>
2026-08-31 16:21:51 -0400

    Time: 31 ms (32.93%), Space: 27.4 MB (27.19%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,18 +1,27 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        """we can have two arrays prod of right and prod of left to save us from 
-        recomputing the product everytime. then we multiply these with eachother. 
-        To optimize this more, we can sequentially do left first and then right or vice-versa  
-        """
-
-        n = len(nums)
-        res = [1]*n
-        prod = 1
-        for i in range(1,n):
-            res[i] = res[i-1]*nums[i-1]
-
-        for i in range(n-2,-1,-1):
-            prod = prod*nums[i+1]
-            res[i] = res[i]*prod
         
-        return res
+        n = len(nums)
+        l = [1]* n
+        r = [1]* n
+
+        prod = 1
+
+        for i in range(1, n):
+            prod = nums[i-1]* prod
+            l[i] = prod
+
+        # print(l)
+
+        prod = 1
+
+        for i in range(n-2, -1, -1):
+            prod = nums[i+1]*prod
+            r[i] = prod
+
+        print(r)
+
+        for i in range(n):
+            nums[i] = r[i]*l[i]
+
+        return nums 
