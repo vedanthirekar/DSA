dadfe2a6d8a31c6cbf806fbbd14c6fe0c666f48c
Vedant Hirekar <vedanthirekar@gmail.com>
2026-09-21 11:51:54 -0400

    Time: 211 ms (92.47%), Space: 19.3 MB (36.94%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,31 +1,30 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        n = len(s)
-        res = ""
+        
         longest = 0
-        for i in range(n):
-            curr_longest = 1
+        res = ""
+
+        for i in range(len(s)):
             l = i-1
             r = i+1
-            while l>=0 and r<n and s[l] == s[r]:
-                curr_longest += 2 
-                l-=1 
-                r+=1
-            if curr_longest> longest:
-                res = s[l+1:r]
-                longest = curr_longest
+            while l>=0 and r<len(s) and s[l] == s[r]:
+                l -=1
+                r +=1
 
-        for i in range(n-1):
-            if s[i] == s[i+1]:
-                curr_longest = 2
-                l = i-1
-                r = i+2
-                while l>=0 and r<n and s[l] == s[r]:
-                    curr_longest += 2 
-                    l-=1 
-                    r+=1
-                if curr_longest> longest:
-                    res = s[l+1:r]
-                    longest = curr_longest
+            if r-l-1>longest:
+                longest = r-l-1
+                res = s[l+1: r]
+
+
+            l = i
+            r = i+1
+            while l>=0 and r<len(s) and s[l] == s[r]:
+                l -=1
+                r +=1
+
+            if r-l-1>longest:
+                longest = r-l-1
+                res = s[l+1: r]
+
 
         return res
