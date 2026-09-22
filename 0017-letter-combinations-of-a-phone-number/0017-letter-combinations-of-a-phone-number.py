1f9af245c62cd63a716a0358f6dd387c76257862
Vedant Hirekar <vedanthirekar@gmail.com>
2026-09-22 10:45:46 -0400

    Time: 0 ms (100%), Space: 19.4 MB (41.88%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,22 +1,20 @@
 class Solution:
-    def letterCombinations(self, digits: str) -> List[str]:
-        mapper = {"2":"abc", "3":"def", "4":"ghi", "5":"jkl", "6":"mno", '7':"pqrs", '8':"tuv", '9':"wxyz"}
-        n = len(digits)
-        ans = []
-        res = [] 
+    def letterCombinations(self, digits: str) -> list[str]:
+        
 
-        def dfs(ans, i, curr_len):
-            if curr_len == n:
-                res.append("".join(ans[:]))
+        mapping = {"2":"abc","3":"def", "4":"ghi", "5":"jkl", "6":"mno", "7":"pqrs", "8":"tuv", "9":"wxyz"}
+        res = []
+        curr = []
+        def backtrack(curr, i):
+            
+            if i == len(digits):
+                res.append("".join(curr))
                 return
 
-            for char in mapper[digits[i]]:
-                ans.append(char)
-                dfs(ans, i+1, curr_len+1)
-                ans.pop()
-            
+            for letter in mapping[digits[i]]:
+                curr.append(letter)
+                backtrack(curr, i+1)
+                curr.pop() 
 
-        dfs([],0, 0)
+        backtrack(curr, 0)
         return res
-
-        
